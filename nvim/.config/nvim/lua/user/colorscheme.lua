b2e35ddea44f85edcb058e499b59d37cfb6cd437
kesalohe <kesalohe@proton.me>
2022-07-21 10:42:37 +0200

    Correctly use light version of vscode colorscheme

--- nvim/.config/nvim/lua/user/colorscheme.lua
+++ nvim/.config/nvim/lua/user/colorscheme.lua
@@ -1,4 +1,21 @@
-vim.cmd([[ colorscheme vscode ]])
+-- vim.cmd([[ colorscheme vscode ]])
 -- vim.o.background = "dark"
 vim.o.background = "light"
+
+local vscode_status_ok, vscode = pcall(require, "vscode")
+if not vscode_status_ok then
+  return
+end
+
+-- local c = require('vscode.colors')
+require('vscode').setup({
+    -- Enable transparent background
+    transparent = true,
+
+    -- Enable italic comment
+    italic_comments = true,
+
+    -- Disable nvim-tree background color
+    disable_nvimtree_bg = true,
+})
 
